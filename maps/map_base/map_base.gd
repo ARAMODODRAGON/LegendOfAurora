9192063d045cc2dc580e9cea044b8f7cc552bea3
Domara Shlimon <domara2000@icloud.com>
2025-08-04 23:31:33 -0400

    fixed screen transition bug

--- maps/map_base/map_base.gd
+++ maps/map_base/map_base.gd
@@ -2,31 +2,35 @@ extends Node
 class_name MapBase
 
 @export_category("Screen Transitions")
-@export var TRANSITION_TIME : float
+@export var TRANSITION_TIME: float
 
 @export_category("Other")
 # TODO: spawn player manually instead of adding them in the inspector
 @export var player: Player = null
 
 ## base map references
-@onready var game_camera: GameCamera = $GameCamera
+@onready var camera: Camera2D = $Camera
 @onready var entities_layer: Node = $"Layers/Entities Layer"
 
 ## tweens
-var _tween: Tween = null
+var tween: Tween = null
 
 func _ready() -> void:
-	game_camera.visible = true
+	camera.visible = true
 
 func _process(_delta: float) -> void:
 	_check_player_room_change()
-	pass
 
 func _check_player_room_change() -> void:
+	## sanity check
 	if player == null: return
 	
-	var rect: Rect2 = game_camera.get_viewport_rect()
-	rect.position += game_camera.position
+	## check if there is a screen transition running
+	if tween != null: return
+
+	## the screen we are on
+	var rect: Rect2 = camera.get_viewport_rect()
+	rect.position += camera.position
 	
 	## we exit early if the player hasnt left this room
 	if rect.has_point(player.position): return
@@ -34,7 +38,7 @@ func _check_player_room_change() -> void:
 	## now if they have we need to check which direction
 	var direction: Vector2 = Vector2.ZERO
 
-	if rect.position.x >= player.position.x: 
+	if rect.position.x >= player.position.x:
 		direction.x = -1.0
 	elif rect.end.x <= player.position.x:
 		direction.x = 1.0
@@ -58,7 +62,7 @@ func _check_player_room_change() -> void:
 ## called to move the camera + player to a new screen while spawning in entities and unloading the last screen
 func move_screens_coroutine(direction: Vector2, screen_rect: Rect2) -> void:
 	## determine the camera and player's new position
-	var new_camera_position: Vector2 = game_camera.position + (direction * Util.SCREEN_SIZE)
+	var new_camera_position: Vector2 = camera.position + (direction * Util.SCREEN_SIZE)
 	var new_player_position: Vector2 = Util.nearest_point_within_rect(player.position, screen_rect) + (direction * Util.TILE_SIZE * 0.5)
 
 	## pre tween functionality (TODO: pause entities on screen and load the next set of entities)
@@ -67,19 +71,23 @@ func move_screens_coroutine(direction: Vector2, screen_rect: Rect2) -> void:
 	#region tween logic
 
 	## now we tween
-	_tween = create_tween()
+	tween = create_tween()
 
 	## the camera
-	_tween.tween_property(game_camera, "position", new_camera_position, TRANSITION_TIME)
+	tween.tween_property(camera, "position", new_camera_position, TRANSITION_TIME)
 
 	## the player
-	_tween.parallel().tween_property(player, "position", new_player_position, TRANSITION_TIME)
+	tween.parallel().tween_property(player, "position", new_player_position, TRANSITION_TIME)
 
 	#endregion
 
 	## await this tween as to make this a coroutine
-	await _tween.finished
-	_tween = null
+	await tween.finished
+	tween = null
+
+	var rect: Rect2 = camera.get_viewport_rect()
+	rect.position += camera.position
+	print(rect)
 
 	## post tween functionality
 	player.process_mode = Node.PROCESS_MODE_INHERIT
--- scripts/util.gd
+++ scripts/util.gd
@@ -2,7 +2,7 @@ extends Node
 ## Util class containing common constants / functionality
 
 const TILE_SIZE : Vector2 = Vector2(16.0, 16.0)
-const SCREEN_SIZE : Vector2 = Vector2(256.0, 240.0)
+const SCREEN_SIZE : Vector2 = Vector2(256.0, 224.0)
 
 const VERY_SMALL : float = 0.001
 
