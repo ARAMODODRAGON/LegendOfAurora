75716cf4987030861d39c1bd4b4250f04d7c0ee3
Domara Shlimon <domara2000@icloud.com>
2025-08-25 20:36:08 -0400

    restructuring for open world generation

--- objects/game_camera/game_camera.gd
+++ objects/game_camera/game_camera.gd
@@ -1,2 +1,12 @@
 extends Camera2D
 class_name GameCamera
+
+@export var player_reference: Player
+
+func _process(delta: float) -> void:
+	if player_reference: _follow_player(delta)
+
+
+func _follow_player(delta: float) -> void:
+	
+	position = player_reference.position
