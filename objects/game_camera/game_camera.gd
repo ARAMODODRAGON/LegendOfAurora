998606632801e3a3a76ea2edad288c8833d11b99
Domara Shlimon <domara2000@icloud.com>
2025-09-11 00:21:58 -0400

    fixed ui not following the camera and so the fade transition would not work

--- objects/game_camera/game_camera.gd
+++ objects/game_camera/game_camera.gd
@@ -8,6 +8,7 @@ var _shake_scale: float = 0.0
 var _shake_duration: float = 0.0
 var _offset: Vector2
 
+var _tween : Tween = null
 
 func shake_screen(scale_: float, duration: float) -> void:
 	_shake_scale = scale_
@@ -16,23 +17,26 @@ func shake_screen(scale_: float, duration: float) -> void:
 
 ## call to fade, false to fade out or true to fade in
 func transition_fade_coroutine(time: float, fade_to_black: bool) -> void:
-	const BLACK := Color(0.0, 0.0, 0.0, 1.0)
-	const TRANSPARENT := Color(0.0, 0.0, 0.0, 0.0)
-	
-	_fade_rect.color = TRANSPARENT if fade_to_black else BLACK
-	var final_color: Color = BLACK if fade_to_black else TRANSPARENT
+	if _tween:
+		printerr("Could not transition as there was already a transition running")
+		return
 
-	var tween := create_tween()
+	_fade_rect.color.a = 0.0 if fade_to_black else 1.0
+	var final_alpha : float = 1.0 if fade_to_black else 0.0
+
+	_tween = create_tween()
 
 	if not fade_to_black:
-		tween.tween_interval(_fade_delay)
+		_tween.tween_interval(_fade_delay)
 
-	tween.tween_property(_fade_rect, "color", final_color, time)
+	_tween.tween_property(_fade_rect, "color:a", final_alpha, time)
 
 	if fade_to_black:
-		tween.tween_interval(_fade_delay)
+		_tween.tween_interval(_fade_delay)
 		
-	await tween.finished
+	await _tween.finished
+
+	_tween = null
 
 
 func _ready() -> void:
